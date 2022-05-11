b10f5c4b2ec97f067ea202a3a60883b54ef1e1d1
Tommy Smith <tommy@tommyesmith.com>
2022-05-11 16:46:05 -0400

    add static IP address

--- __main__.py
+++ __main__.py
@@ -4,12 +4,21 @@ import pulumi_aws as aws
 # Create a new Lightsail Key Pair
 lg_key_pair = aws.lightsail.KeyPair("lgKeyPair")
 
-# Create a new Wordopress Lightsail Instance
-wordpress_test = aws.lightsail.Instance("aws-lightsail-deployment",
-    availability_zone="us-east-1b",
+# Create a static IP address
+static_ip = aws.lightsail.StaticIp("StaticIp")
+
+# Create a new WordPress Lightsail Instance
+new_instance = aws.lightsail.Instance(
+    "aws-lightsail-deployment",
     blueprint_id="wordpress",
+    availability_zone="us-east-1b",
     bundle_id="nano_2_0",
     key_pair_name=lg_key_pair.id,
     tags={
         "Project": "Wordpress Deploy",
     })
+
+static_ip_attachment = aws.lightsail.StaticIpAttachment(
+    "StaticIpAttachment",
+    static_ip_name=static_ip.id,
+    instance_name=new_instance.id)
